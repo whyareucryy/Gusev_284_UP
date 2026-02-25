dc5198576a10a32e3938b660a786c288dcb93ba9
Gusev Mihail <mihailfranks@gmail.com>
2026-02-26 01:54:11 +0500

    Создание проекта в VS

---
+++ VS code/Gusev284_UP/Gusev284_UP/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Gusev284_UP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Gusev284_UP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VS code/Gusev284_UP/Gusev284_UP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gusev284_UP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
